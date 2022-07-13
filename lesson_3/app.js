cfb2b598bb07a60d46886c944c7446f28d1fe6f7
Dzhaokan <ikanat1993@gmail.com>
2022-07-13 18:58:00 +0600

    lesson_3

--- lesson_3/app.js
+++ lesson_3/app.js
@@ -25,17 +25,26 @@
 //
 // anonym();
 
-function twoMass (arr1, arr2) {
-    if(arr1.length > arr2.length){
-        alert("arr1 > arr2")
-    }else if(arr1.length < arr2.length){
-        alert("arr1 < arr2")
-    }else {
-        alert("arr1 === arr2")
+// function twoMass (arr1, arr2) {
+//     if(arr1.length > arr2.length){
+//         alert("arr1 > arr2")
+//     }else if(arr1.length < arr2.length){
+//         alert("arr1 < arr2")
+//     }else {
+//         alert("arr1 === arr2")
+//     }
+//
+// }
+// const arr1 = [1212, 32423, 4234]
+// const arr2 = [23432, 234, 23, 432, 23432, 234]
+//
+// twoMass(arr1, arr2)
+
+function tr () {
+    let line = "*"
+    while(line.length <= 7){
+        console.log(line)
+        line = line + "*"
     }
-
 }
-const arr1 = [1212, 32423, 4234]
-const arr2 = [23432, 234, 23, 432, 23432, 234]
-
-twoMass(arr1, arr2)
+tr()
