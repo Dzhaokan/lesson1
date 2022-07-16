366f368a86f3e1c15c13fcdfd81cb303056da1a1
Dzhaokan <ikanat1993@gmail.com>
2022-07-16 18:29:56 +0600

    lesson_4

---
+++ lesson_4/main.js
@@ -0,0 +1,5 @@
+const blocks = document.getElementsByClassName("block") //брать элемент по классу
+// console.log(blocks)
+
+const block3 = document.getElementById("block3") // брать элемент по id
+console.log(block3)
