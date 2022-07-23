cfa6d13c8923261894b3351768c3ca54dd9ce1e1
Dzhaokan <ikanat1993@gmail.com>
2022-07-23 19:01:12 +0600

    lesson_6

---
+++ lesson_6/app.js
@@ -0,0 +1,62 @@
+//es5 and es6
+
+//string expression - интерполяция строк
+
+const first_name = "Jack";
+const last_name = "Barbaro";
+
+console.log("Full Name" + ' ' + first_name + ' ' + last_name)
+
+console.log(`Full Name: ${first_name} ${last_name}`)
+
+const getFullName = (user) => {
+    // return "First Name: " + user.first_name + "\n" + "Last Name: " + user.last_name
+    return `First Name: ${user.first_name}\nLast Name: ${user.last_name}`
+}
+
+const user = {
+    first_name: 'Atay',
+    last_name: 'Bekturov'
+}
+console.log(getFullName(user))
+
+//spread operator
+
+const mass = [1,2,3,4,5]
+const mass2 = [...mass]
+
+// for (let i = 0; i < mass.length; i++){
+//     mass2.push(mass[i])
+// }
+console.log(mass2)
+
+const obj = {
+    username: "beks",
+    age: 18,
+    last_name: "Bekmamatov",
+    full_name: 'Beks Bekmamatov'
+}
+const obj2 = {...obj}
+console.log(obj2)
+
+//деструктуризация
+
+const props2 = {
+    onSubmit: () => {
+        console.log("submit")
+    },
+    data: [
+        {
+            key: 'value'
+        },
+        {
+            key: 'value'
+        },
+        {
+            key: 'value'
+        }
+    ]
+}
+const {data, onSubmit} = props2;
+console.log(data)
+onSubmit ()
